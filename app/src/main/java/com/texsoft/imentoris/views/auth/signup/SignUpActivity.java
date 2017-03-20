8d8ab53088fc402c1d1e637e6e73465f69b59b37
RogerioTeixeira <rogerio.teixeira@hotmail.it>
2017-03-20 08:11:12 +0100

    Commit casa

--- app/src/main/java/com/texsoft/imentoris/views/auth/signup/SignUpActivity.java
+++ app/src/main/java/com/texsoft/imentoris/views/auth/signup/SignUpActivity.java
@@ -1,12 +1,25 @@
 package com.texsoft.imentoris.views.auth.signup;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 
 import com.texsoft.imentoris.R;
 import com.texsoft.imentoris.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class SignUpActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    public static void startActivity(Activity activity) {
+        Intent intent = new Intent(activity, SignUpActivity.class);
+        activity.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +28,10 @@ public class SignUpActivity extends BaseActivity {
             Fragment fragment = new SignUpFragment();
             addFragment(R.id.fragmentContainer, fragment);
         }
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 
     @Override
